23823cc9b902689bf5d356c98744f2dfb34a9ece
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2018-11-14 13:15:45 +0300

    cleanup

--- MyApplication/app/src/main/java/com/example/podvorniy/myapplication/NewsModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.podvorniy.myapplication
-
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
-
-
-class NewsModell(val photo:Bitmap, val text: String )
--- Project_Summmer_2018/Pods/Target Support Files/Pods-Project_Summmer_2018/Pods-Project_Summmer_2018-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Project_Summmer_2018 : NSObject
-@end
-@implementation PodsDummy_Pods_Project_Summmer_2018
-@end
