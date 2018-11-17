664e498d628be9c0fd4976af2c786f947d5726f9
FixedOctocat <EgorFilatov2002@yandex.ru>
2018-11-17 15:43:33 +0300

    templates

--- server/instance/interface/controllers.py
+++ server/instance/interface/controllers.py
@@ -3,16 +3,30 @@ from instance.database import db
 
 module = Blueprint('interface', __name__, url_prefix='')
 
-@module.route('/main', methods=['GET', 'POST'])
+@module.route('/menu', methods=['GET', 'POST'])
 def main():
     if request.method == 'GET':
-        return render_template('/templates/MainMenuP.html')
+        return render_template('MainMenuP.html')
     elif request.method == 'POST':
         pass
 
 @module.route('/time_table', methods=['GET', 'POST'])
 def time_table():
     if request.method == 'GET':
-        return render_template('/templates/TimeTableP.html')
+        return render_template('TimeTableP.html')
+    elif request.method == 'POST':
+        pass
+
+@module.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'GET':
+        return render_template('Authorization.html')
+    elif request.method == 'POST':
+        pass
+
+@module.route('/school_event/add', methods=['GET', 'POST'])
+def school_event_add():
+    if request.method == 'GET':
+        return render_template('add_school_event.html')
     elif request.method == 'POST':
         pass
--- server/newserver/config.py
+++
@@ -1,3 +0,0 @@
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-DEBUG = False
-SQLALCHEMY_DATABASE_URI = "sqlite://///home/fixed/Desktop/projects/SchoolToday/server/newserver/SchoolToday.sqlite3"
