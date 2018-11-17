fed0077ebba9288a2b704add2b554267270ad648
FixedOctocat <EgorFilatov2002@yandex.ru>
2018-11-17 15:45:14 +0300

    ...

---
+++ server/instance/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from .database import db
+
+
+def create():
+    SchoolToday = Flask(__name__)
+    SchoolToday.config.from_object('config')
+    SchoolToday.config.from_pyfile('config.py')
+    db.init_app(SchoolToday)
+    with SchoolToday.test_request_context():
+        db.create_all()
+
+    import instance.feed as feed
+    SchoolToday.register_blueprint(feed.module)
+    import instance.schedule as schedule
+    SchoolToday.register_blueprint(schedule.module)
+    import instance.interface as interface
+    SchoolToday.register_blueprint(interface.module)
+    return SchoolToday
---
+++ server/instance/schedule/controllers.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, request, Response, jsonify
+import gspread
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+
+module = Blueprint('schedule', __name__, url_prefix='/api/schedule')
+
+
+def FromBadToGoodWord(str1):
+    return str1.split("'")[1]
+
+
+@module.route('/get_schedule/<cl>', methods=['GET'])
+def GetTimeTable(cl):
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('1.json', scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open_by_url(
+        'https://docs.google.com/spreadsheets/d/1yI8crMmfMbCFQMx5PdqBreCjTNK5r-aJeqgU9pWN85c/edit#gid=1539915031')
+    g = wks.worksheet('расписание')
+    cls = {'5C': 'C', '6C': 'D', '7C': 'E', '7T': 'F', '7L': 'G', '9C': 'H'}
+    cell_list = g.range(cls[cl] + '2:' + cls[cl] + '72')
+    ans_list = [[], [], [], [], [], []]
+    for i in range(2, 71):
+        if i < 14:
+            ans_list[0].append(FromBadToGoodWord(str(cell_list[i])))
+        if i > 13 and i < 25:
+            ans_list[1].append(FromBadToGoodWord(str(cell_list[i])))
+        if i > 24 and i < 38:
+            ans_list[2].append(FromBadToGoodWord(str(cell_list[i])))
+        if i > 37 and i < 49:
+            ans_list[3].append(FromBadToGoodWord(str(cell_list[i])))
+        if i > 48 and i < 62:
+            ans_list[4].append(FromBadToGoodWord(str(cell_list[i])))
+        if i > 61:
+            ans_list[5].append(FromBadToGoodWord(str(cell_list[i])))
+    return json.dumps({'ans': ans_list})
