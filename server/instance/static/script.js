782ced604beb7449a790bca7b0f9654538e9ae1e
dimacheburek <dpro0406@yandex.ru>
2018-12-01 12:57:48 +0300

    23erfsdc

--- server/instance/static/script.js
+++ server/instance/static/script.js
@@ -1,18 +1,29 @@
-function raa() {
-    var tbl = '{"subject":"Русский","food":false,"place": 409,"comment":"Yarik"}';
-    tbl = JSON.parse(tbl);
-    document.getElementById('nrop').innerHTML += tbl.subject;
-    document.getElementById('nrop').innerHTML += tbl.food;
-    document.getElementById('nrop').innerHTML += tbl.place;
-    document.getElementById('nrop').innerHTML += tbl.comment;
-    document.getElementById('nrop').appendChild;
+function tableCreate(data) {
+    var body = document.getElementById('nrop');
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < 12; i++) {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        td.innerText = data[i];
+        tr.appendChild(td);
+        tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl)
 }
 
-function kyky() {
-    jQuery.ajax({
+function gtt() {
+    $.ajax({
         url: 'http://127.0.0.1:5000/api/schedule/get_schedule/5C',
-        success: function () {
-            alert('Load was performed.');
+        success: function (data) {
+            data = JSON.parse(data);
+            console.log(data.ans[0, 0]);
+            // document.getElementById('nrop').innerHTML = data.ans[0, 0];
+            tableCreate(data.ans[0]);
         }
     });
 }
+window.onload = gtt;
