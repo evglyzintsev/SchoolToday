61c1b419121e4ebdcee4a795308ed75c3bf6af6f
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-01-21 17:31:41 +0300

    init AndroidApp

---
+++ AndroidSchoolToday/app/src/main/java/com/example/podvorniy/schooltodayandroid/LoginView.kt
@@ -0,0 +1,12 @@
+package com.example.podvorniy.schooltodayandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loginview)
+    }
+}
